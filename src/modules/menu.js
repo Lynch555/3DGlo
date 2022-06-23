008f774332146631c989704b4c8060c369c1ab9a
Сергей <vishneuski1983@mail.ru>
2022-06-23 19:53:21 +0300

    lesson20hard

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,17 +3,43 @@ const menu = () => {
     const menu = document.querySelector('menu')
     const closeBtn = menu.querySelector('.close-btn')
     const menuItems = menu.querySelectorAll('ul>li>a')
+    const miniLink = document.querySelector('main>a')
 
-    const handleMenu = () =>{
+    const handleMenu = () => {
         menu.classList.toggle('active-menu')
-
     }
 
     menuBtn.addEventListener('click', handleMenu)
-    closeBtn.addEventListener('click', handleMenu)  
-    
-    menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
-    
+    closeBtn.addEventListener('click', handleMenu)
+
+    // УСЛОЖНЕННОЕ ЗАДАНИЕ
+
+    menuItems.forEach(menuItem => {
+        menuItem.addEventListener('click', (event) => {
+            handleMenu() 
+            event.preventDefault(); 
+
+            let href = menuItem.getAttribute('href').substring(1); 
+            const scrollTarget = document.getElementById(href) 
+            const elementPosition = scrollTarget.getBoundingClientRect().top; 
+
+            window.scrollBy({ 
+                top: elementPosition,
+                behavior: "smooth"
+            })
+        })
+    });
+
+    miniLink.addEventListener('click', (event) => {  
+        const scrollTarget = document.getElementById('service-block')
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        event.preventDefault();
+
+        window.scrollBy({
+            top: elementPosition,
+            behavior: "smooth"
+        })
+    })
 }
 
 export default menu
