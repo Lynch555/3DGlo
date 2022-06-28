4eb1a4dc9a0f2818613f69cd435bb06d665a1e10
Lynch555 <vishneuski1983@mail.ru>
2022-06-28 21:51:38 +0300

    lesson22

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,14 +1,13 @@
 const modal = () => {
     const modal = document.querySelector('.popup');
     const buttons = document.querySelectorAll('.popup-btn');
-    const closeBtn = document.querySelector('.popup-close');
     const modalContent = modal.querySelector('.popup-content');
 
     let count = -100;
     let animationId;
 
     const modalAnimate = () => {
-        count += 2;
+        count += 5;
         animationId = requestAnimationFrame(modalAnimate);
         modal.style.display = 'block';
 
@@ -19,7 +18,7 @@ const modal = () => {
         }
     };
 
-      buttons.forEach(btn => {
+    buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             if (screen.width < 768) {
                 modalContent.style.top = 10 + '%';
@@ -30,12 +29,13 @@ const modal = () => {
         });
     });
 
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none';
-        count = -100;
-        modalContent.style.top = -100 + '%';
+    modal.addEventListener('click', (e) => {
+        if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+            modal.style.display = 'none';
+            count = -100;
+            modalContent.style.top = -100 + '%';
+        }
     });
-
 };
 
 export default modal;
