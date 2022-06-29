cdae043fcb607f46a2c1689cb5c1feecc919535a
Lynch555 <vishneuski1983@mail.ru>
2022-06-29 10:04:44 +0300

    lesson23

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import forms from "./modules/forms";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer('06 october 2022');
 menu();
 modal();
 forms();
 tabs();
+slider();
