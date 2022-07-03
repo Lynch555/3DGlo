c86f2e965288219a6f0fa0881adcb2b669c51c71
Lynch555 <vishneuski1983@mail.ru>
2022-07-03 22:25:29 +0300

    lesson25

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import forms from "./modules/forms";
+import tabs from "./modules/tabs";
+import slider from "./modules/slider";
+import calc from "./modules/calc";
 
 timer('06 october 2022');
 menu();
 modal();
 forms();
+tabs();
+slider();
+calc(100);
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,41 +1,36 @@
+//import { AsyncDependenciesBlock } from 'webpack';
+import { animate } from './helpers';
+
 const modal = () => {
-    const modal = document.querySelector('.popup');
-    const buttons = document.querySelectorAll('.popup-btn');
-    const closeBtn = document.querySelector('.popup-close');
-    const modalContent = modal.querySelector('.popup-content');
+  const modal = document.querySelector('.popup');
+  const buttons = document.querySelectorAll('.popup-btn');
+  const popupContent = modal.querySelector('.popup-content');
 
-    let count = -100;
-    let animationId;
-
-    const modalAnimate = () => {
-        count += 2;
-        animationId = requestAnimationFrame(modalAnimate);
-        modal.style.display = 'block';
-
-        if (count <= 10) {
-            modalContent.style.top = count + '%';
-        } else {
-            cancelAnimationFrame(animationId);
-        }
-    };
-
-      buttons.forEach(btn => {
-        btn.addEventListener('click', () => {
-            if (screen.width < 768) {
-                modalContent.style.top = 10 + '%';
-                modal.style.display = 'block';
-            } else {
-                modalAnimate();
-            }
-        });
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      popupContent.style.top = 0;
+      modal.style.display = 'block';
+      
+      animate({
+        duration: 350,
+        timing(timeFraction) {
+          return timeFraction;
+        },
+        draw(progress) {
+            popupContent.style.top = (25 * progress) + "%";
+        },
+      });
     });
+  });
 
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none';
-        count = -100;
-        modalContent.style.top = -100 + '%';
-    });
-
+  modal.addEventListener('click', (e) => {
+    if (
+      !e.target.closest('.popup-content') ||
+      e.target.classList.contains('popup-close')
+    ) {
+      modal.style.display = 'none';
+    }
+  });
 };
 
 export default modal;
